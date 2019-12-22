6d7610d8d858396c2d1f9216a8c993e4faa163d5
Dejan Zlataric <dejanzlataric@outlook.com>
2019-12-22 10:11:19 +0100

    Generated paypal-payment component for client app

--- pc-client/src/app/app.module.ts
+++ pc-client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaypalPaymentComponent } from './paypal-payment/paypal-payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaypalPaymentComponent
   ],
   imports: [
     BrowserModule
