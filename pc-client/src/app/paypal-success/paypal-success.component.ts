b1c5b45c9dd2c9aa5dc96c325165168086d65c31
Dejan Zlataric <dejanzlataric@outlook.com>
2019-12-28 03:57:33 +0100

    Chose payment type by clicking on a button

--- pc-client/src/app/paypal-success/paypal-success.component.ts
+++ pc-client/src/app/paypal-success/paypal-success.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ExecutePayment} from "../types/executepayment";
-import {PaypalResponse} from "../types/paypalresponse";
 import {HttpClient} from '@angular/common/http';
 
 
