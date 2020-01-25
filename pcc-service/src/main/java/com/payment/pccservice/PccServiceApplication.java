38a745cbe4e0e5104094e57d25d687bd803f439a
dobrica <djdobrica@gmail.com>
2020-01-25 21:30:55 +0100

    Added new service - PCC
    - empty service that will be used as PCC service for interbank communication

---
+++ pcc-service/src/main/java/com/payment/pccservice/PccServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payment.pccservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccServiceApplication.class, args);
+	}
+
+}
---
+++ pcc-service/src/test/java/com/payment/pccservice/PccServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.pccservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PccServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
